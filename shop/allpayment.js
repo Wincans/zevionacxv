441fe05830e35ac4274e89c1442df511ed1d89af
Wincans <94646426+Wincans@users.noreply.github.com>
2021-11-26 23:30:52 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,27 +1,16 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
-
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
+return `PAYMENT BY ZEVIONA OFC
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ +1 (231) 299 6002
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ +1 (231) 299 6002
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ +1 (231) 299 6002
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama ZE****CT
 
 *📲 PULSA*
 
